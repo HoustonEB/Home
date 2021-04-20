a9999430f058b4354bf55732dd0bfd6e0577a912
HoustonEB <860852345@qq.com>
2021-04-20 19:55:21 +0800

    添加密码4

--- scripts/deployServer.js
+++ scripts/deployServer.js
@@ -1,6 +1,8 @@
 const SftpConnection = require('sftp-connection');
+const p = require('path');
 const { readFileSync } = require('fs');
 const password = process.argv[2];
+
 console.log(password, 'password---------')
 const sftp = new SftpConnection({
     host: '180.76.58.86',
@@ -14,6 +16,6 @@ console.log(localPathPrefix, 'localPathPrefix------')
 sftp.upload({
     localPathPrefix,
     // localPath: localPathPrefix + '{?(.nuxt|static|utils|plugins)/**,.npmrc,nuxt.config.js,package.json}',
-    localPath: localPathPrefix + '{output/output.zip}',
+    localPath: p.join(localPathPrefix + '../' + '/output/output.zip}'),
     remotePath: '/usr/local/nginx/html/home-node-server/'
 });
