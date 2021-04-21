577b58a6b12b76631d1ac35811bd5deda5fbd169
HoustonEB <860852345@qq.com>
2021-04-21 17:03:43 +0800

    修改部署脚本8

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,11 +7,12 @@ run() {
 }
 echo $SERVERPASS
 run "ls"
+run "echo "$(date +%Y%m/%d%H%M)" > ./pm2/version.json"
 run "npm ci"
 run "npm run build"
-run "rm -rf output"
-run "mkdir output"
-run "zip -rq ./output/output.zip ../Home"
-run "ls"
+# run "rm -rf output"
+# run "mkdir output"
+# run "zip -rq ./output/output.zip ../Home"
+# run "ls"
 # shell中脚本执行的路径相对于shell执行时的路径,所以./deployServer.js会报找不到模块
 run "node ./scripts/deployServer.js $SERVERPASS"
